7e93800b23982eae3bf5ab0ef8344f92995fa5d0
SonicFanXD <jonicgamer08@gmail.com>
2026-08-30 15:29:29 -0600

    Fix: add AuroraPlayerApp.swift and update project.yml

--- AuroraPlayerApp.swift
+++ AuroraPlayerApp.swift
@@ -2,25 +2,9 @@ import SwiftUI
 
 @main
 struct AuroraPlayerApp: App {
-    @StateObject private var audioEngine = AudioEngine()
-    @Environment(\.scenePhase) private var scenePhase
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(audioEngine) // Opcional, pero lo dejamos para futuro
-        }
-        .onChange(of: scenePhase) { newPhase in
-            switch newPhase {
-            case .background, .inactive:
-                // Guardar estado cuando la app pase a segundo plano
-                audioEngine.saveState()
-            case .active:
-                // No hacemos nada al volver, ya se restaura en ContentView
-                break
-            @unknown default:
-                break
-            }
         }
     }
 }
